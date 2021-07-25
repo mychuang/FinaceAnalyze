c70c38510011d1566a00703764493f4865336e44
mychuang <hhjoy222@gmail.com>
2021-07-25 11:31:54 +0800

    add the volum plot

--- stock.py
+++ stock.py
@@ -11,16 +11,16 @@ from FinMind.data import DataLoader
 
 dl = DataLoader()
 # 下載台股股價資料
-stock_data = dl.taiwan_stock_daily(
+stockData = dl.taiwan_stock_daily(
     stock_id='2330', start_date='2020-07-01', end_date='2021-07-24'
 )
 # 下載三大法人資料
-stock_data = dl.feature.add_kline_institutional_investors(
-    stock_data
+stockData = dl.feature.add_kline_institutional_investors(
+    stockData
 ) 
 # 下載融資券資料
-stock_data = dl.feature.add_kline_margin_purchase_short_sale(
-    stock_data
+stockData = dl.feature.add_kline_margin_purchase_short_sale(
+    stockData
 )
 
 #%%
@@ -32,21 +32,18 @@ import matplotlib.ticker as ticker
 
 #plot open & close
 fig, ax = plt.subplots(figsize=(16,8))
-ax.plot(stock_data['date'], stock_data['close'], color='red',label='close')
-ax.plot(stock_data['date'], stock_data['open'], color='blue',label='open')
-#ax.plot(stock_data .close, color='red',label='close')
-#ax.plot(stock_data .open,color='green',label='open')
-ax.xaxis.set_major_locator(ticker.MultipleLocator(5))
+ax.plot(stockData['date'], stockData['close'], color='red',label='close')
+ax.plot(stockData['date'], stockData['open'], color='blue',label='open')
+#ax.bar(stock_data[Trading_Volume])
+ax.xaxis.set_major_locator(ticker.MultipleLocator(10))
+
 ax.minorticks_on()
+ax.grid(which='minor', axis='both')
+ax.xaxis.set_tick_params(rotation=45,labelsize=10,colors='g')  
+ax.yaxis.tick_right()
 
+ax.set_title('2330 daily-K',fontsize=18)
+ax.set_xlabel('date', fontsize=18,fontfamily = 'sans-serif',fontstyle='italic')
+ax.set_ylabel('stock proce', fontsize='x-large',fontstyle='oblique')
+ax.legend(fontsize=20)
 
-
-# ax.grid(which='minor', axis='both')
-
-ax.xaxis.set_tick_params(rotation=45,labelsize=12,colors='g')  
-# ax.yaxis.tick_right()
-
-# ax.set_title('2330 daily-K',fontsize=18)
-# ax.set_xlabel('date', fontsize=18,fontfamily = 'sans-serif',fontstyle='italic')
-# ax.set_ylabel('stock proce', fontsize='x-large',fontstyle='oblique')
-# ax.legend()
